22812675d8a85ea1211ee858a3cd645908e04081
par-adigm <paradigm.growing@gmail.com>
2026-09-24 09:21:17 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10573047f75374ff669ad2bf77540951468881c2f2f2f1c172fc31ecbd5e585c
-size 328
+oid sha256:49294566613ee4306dfa6b3007df7c9f75ec0fa394a0f294ae90892deb76d309
+size 560
